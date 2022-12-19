03ea8308cbb67e9f3c1ff459e39b5ad0c315a3c0
v.shorinadm <voldemar-96@mail.ru>
2022-12-19 18:24:59 +1000

    Форматирование кода

--- src/api/api.js
+++ src/api/api.js
@@ -1,33 +1,35 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: `https://api.unsplash.com/`,
-    headers: {
-        Authorization: "Bearer Hrr5-Kb5FEPmrb74V4mD-AyChtsKL8-a7_i0dSPQRoc"
-    }
-})
+  baseURL: `https://api.unsplash.com/`,
+  headers: {
+    Authorization: 'Bearer Hrr5-Kb5FEPmrb74V4mD-AyChtsKL8-a7_i0dSPQRoc',
+  },
+});
 
-export const Auth = () => instance.get('/me').then(response => response.data);
+export const Auth = () => instance.get('/me').then((response) => response.data);
 
 export const imagesAPI = {
+  likeImage: (imageId) => {
+    return instance
+      .post(`/photos/${imageId}/like`)
+      .then((response) => response);
+  },
 
-    likeImage: (imageId) => {
-        return instance.post(`/photos/${imageId}/like`)
-            .then(response => response);
-    },
+  unlikeImage: (imageId) => {
+    return instance
+      .delete(`/photos/${imageId}/like`)
+      .then((response) => response);
+  },
 
-    unlikeImage: (imageId) => {
-        return instance.delete(`/photos/${imageId}/like`)
-            .then(response => response);
-    },
-
-    getImages: (currentPage) => {
-        return instance.get(`photos`, {
-            params: {
-                page: currentPage,
-                per_page: 6
-            }
-        }).then(response => response.data);
-    }
-}
-
+  getImages: (currentPage) => {
+    return instance
+      .get(`photos`, {
+        params: {
+          page: currentPage,
+          per_page: 6,
+        },
+      })
+      .then((response) => response.data);
+  },
+};
